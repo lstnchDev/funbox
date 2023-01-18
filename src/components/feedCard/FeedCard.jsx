5b88c42ac4fbcc8f4df8a2539983fc2a92c81ebf
lstnchDev <freest2334@gmail.com>
2023-01-19 02:53:05 +0500

    created feedCard

--- src/components/feedCard/FeedCard.jsx
+++ src/components/feedCard/FeedCard.jsx
@@ -1,20 +1,36 @@
 import styles from "./style/feedCard.module.scss"
 import catImg from "../../images/cat.svg"
+import { useState } from "react"
 
 
-const FeedCard = ()=>{
+const FeedCard = ({title, description, composition, portionCount, countMouse, weight, availability, selected, onSelected})=>{
+
+  const [selectedState, setSelected] = useState(false)
+
+  const onClickSelected = () => setSelected(!selectedState)
+
+  const selectedDescription = selectedState ? <p className={styles.description}>{description}</p> : <p className={styles.description}>Чего сидишь? Порадуй котэ, <span onClick={onClickSelected} className={styles.btnBuy}>купи.</span></p>
+
+  const disableCard = availability ? "" : styles.disabled
+  const selectedCard = selectedState ? styles.selected : ""
+
+  const feedDescription = availability ? selectedDescription : <p className={styles.disabledDescription}>Печалька, {composition} закончился.</p>
     return (
-      <div>
-          <div>
+      <div className={styles.feed_container}>
+        <div onClick={onClickSelected} className={`${styles.feed_box} ${selectedCard} ${disableCard}`}>
+          <div className={styles.feed_content}>
             <h4>Сказочное заморское яство</h4>
-            <h1>Нямушка</h1>
-            <h2>с фуа-гра</h2>
-            <p>10 порций</p>
-            <p>мышь в подарок</p>
-            <img src={catImg}/>
-            <p>0,5 кг</p>
+            <h1>{title}</h1>
+            <h2>{composition}</h2>
+            <p><span className={styles.count}>{portionCount}</span> порций</p>
+            <p><span className={styles.count}>{countMouse}</span> мыши в подарок</p>
         </div>
-        <p>Чего сидишь? Порадуй котэ, <a>купи.</a></p>
+          <div className={styles.feed_bottom}>
+            <img src={catImg}/>
+            <p className={styles.weight}><span>{weight}</span> кг</p>
+          </div>
+        </div>
+        <div >{feedDescription}</div>
       </div>
     )
 }
